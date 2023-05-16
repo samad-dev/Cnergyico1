a090de16e94dab460f9afe663ea1cebfb8664f3b
samad-dev <abdulsamadq67@gmail.com>
2023-05-16 17:26:40 +0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'dart:ui';
+import 'package:cnergyico/Complaint.dart';
+import 'package:cnergyico/Lube.dart';
+import 'package:cnergyico/OrderPage.dart';
+import 'package:cnergyico/Uniform.dart';
 import 'package:cnergyico/utils/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -39,11 +43,10 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(top: 60,left: 16,right: 16),
+              padding: const EdgeInsets.only(top: 60, left: 16, right: 16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-
                   const SizedBox(height: 50),
                   GridView.count(
                     crossAxisCount: 2,
@@ -53,29 +56,37 @@ class _HomeScreenState extends State<HomeScreen> {
                     physics: const NeverScrollableScrollPhysics(),
                     children: [
                       GestureDetector(
-                        onTap: (){
-                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>AllMapSample()));
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => OrderScreen()));
                         },
                         child: Container(
                           decoration: BoxDecoration(
                             border: Border(
-                              bottom: BorderSide(width: 1.0, color: Colors.white),
-                              right: BorderSide(width: 1.0, color: Colors.white),
+                              bottom:
+                                  BorderSide(width: 1.0, color: Colors.white),
+                              right:
+                                  BorderSide(width: 1.0, color: Colors.white),
                             ),
                             color: Colors.transparent,
                           ),
                           child: Center(
                             child: Column(
                               children: [
-                                SizedBox(height: 50,),
-                                Icon(Icons.bookmark_border_outlined,color: Constants.primarygreen,size: 60,),
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Icon(
+                                  Icons.bookmark_border_outlined,
+                                  color: Constants.primarygreen,
+                                  size: 60,
+                                ),
                                 Text(
                                   'Order',
                                   style: const TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white
-                                  ),
+                                      color: Colors.white),
                                 ),
                               ],
                             ),
@@ -83,13 +94,15 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       GestureDetector(
-                        onTap: (){
-                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>MyListViewScreen()));
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => UniformScreen()));
                         },
                         child: Container(
                           decoration: BoxDecoration(
                             border: Border(
-                              bottom: BorderSide(width: 1.0, color: Colors.white),
+                              bottom:
+                                  BorderSide(width: 1.0, color: Colors.white),
                               left: BorderSide(width: 1.0, color: Colors.white),
                             ),
                             color: Colors.transparent,
@@ -97,15 +110,20 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Center(
                             child: Column(
                               children: [
-                                SizedBox(height: 50,),
-                                Icon(Icons.man_rounded,color: Constants.primarygreen,size: 60,),
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Icon(
+                                  Icons.man_rounded,
+                                  color: Constants.primarygreen,
+                                  size: 60,
+                                ),
                                 Text(
                                   'Uniform',
                                   style: const TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white
-                                  ),
+                                      color: Colors.white),
                                 ),
                               ],
                             ),
@@ -113,30 +131,36 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       GestureDetector(
-                        onTap: (){
-                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>SettingPage()));
+                        onTap: () {
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Lube()));
                         },
                         child: Container(
                           decoration: BoxDecoration(
                             border: Border(
                               top: BorderSide(width: 1.0, color: Colors.white),
-                              right: BorderSide(width: 1.0, color: Colors.white),
+                              right:
+                                  BorderSide(width: 1.0, color: Colors.white),
                             ),
                             color: Colors.transparent,
                           ),
                           child: Center(
                             child: Column(
                               children: [
-
-                                SizedBox(height: 50,),
-                                Icon(Icons.message_outlined,color: Constants.primarygreen,size: 60,),
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Icon(
+                                  Icons.message_outlined,
+                                  color: Constants.primarygreen,
+                                  size: 60,
+                                ),
                                 Text(
                                   'Complaint',
                                   style: const TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white
-                                  ),
+                                      color: Colors.white),
                                 ),
                               ],
                             ),
@@ -144,8 +168,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       GestureDetector(
-                        onTap: (){
-                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>AlertList()));
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => Complaint()));
                         },
                         child: Container(
                           decoration: BoxDecoration(
@@ -158,26 +183,90 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Center(
                             child: Column(
                               children: [
-                                SizedBox(height: 50,),
-                                Icon(Icons.access_time_outlined,color:Constants.primarygreen,size: 60,),
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Icon(
+                                  Icons.access_time_outlined,
+                                  color: Constants.primarygreen,
+                                  size: 60,
+                                ),
                                 Text(
                                   'Lube',
                                   style: const TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white
-                                  ),
+                                      color: Colors.white),
                                 ),
                               ],
                             ),
                           ),
                         ),
                       ),
-
                     ],
-
                   ),
                 ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height/4.4,
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("Current Fuel Rate",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic,color: Colors.black45),),
+                          ),
+
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("PMG",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic,color: Color.fromRGBO(18, 99, 81, 1.0)),),
+                          ),
+                          SizedBox(width: 140,),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("270.00 Rs/Litre",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic,color: Color.fromRGBO(18, 99, 81, 1.0)),),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.black,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("HSD",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic,color: Color.fromRGBO(18, 99, 81, 1.0)),),
+                          ),
+                          SizedBox(width: 140,),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("258.00 Rs/Litre",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic,color: Color.fromRGBO(18, 99, 81, 1.0)),),
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
